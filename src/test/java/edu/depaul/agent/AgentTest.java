71c01fb5e01a4ae1d99df67e6f92fafc65804033
Paul A. Trzyna <ptrzyna@gmail.com>
2015-02-08 17:45:45 -0600

    fix to cobertura

---
+++ src/test/java/edu/depaul/agent/AgentTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.depaul.agent;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author ptrzyna
+ *
+ */
+public class AgentTest implements Agent {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
